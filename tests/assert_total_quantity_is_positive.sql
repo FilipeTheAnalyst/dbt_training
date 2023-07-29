886aad90afce01c315961b99071c009dc8bebda5
kds-student <student@kahandatasolutions.com>
2023-07-29 15:43:35 +0000

    add singular test

---
+++ tests/assert_total_quantity_is_positive.sql
@@ -0,0 +1,8 @@
+-- You can't order a negative amount of products.
+-- This test will return records that is not true (and cause a failure)
+select
+    customer_id,
+    total_quantity
+from {{ ref('customers') }}
+
+where total_quantity > 0
